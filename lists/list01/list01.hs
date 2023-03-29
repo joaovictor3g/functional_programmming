1cb6671407cce70dedeb7fec2143f490935ee4cf
joaovictor3g <jvdias79797@gmail.com>
2023-03-28 22:43:30 -0300

    feat: more list exercises

--- lists/list01/list01.hs
+++ lists/list01/list01.hs
@@ -1,69 +1,178 @@
+-- 1
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 minorOfTwo :: Int -> Int -> Int
 minorOfTwo = min
 
+-- 2
 minorOfThree :: Int -> Int -> Int -> Int
-minorOfThree x y z = min z (min x y) 
+minorOfThree x y z = min z (min x y)
 
+-- 3
 factorial :: Int -> Int
 factorial 0 = 1
 factorial 1 = 1
 factorial n = n * factorial (n - 1)
 
+-- 4
 fibonacci :: Int -> Int
 fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci 2 = 1
-fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)  
+fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
 
+fib :: Int -> Int
+fib n
+  | n == 0 = 1
+  | n == 1 = 1
+  | n == 2 = 1
+  | otherwise = fib (n - 1) + fib (n - 2)
+
+
+-- 5
 element :: Int -> [Int] -> Int
 element n u = u !! n
 
+-- 6
 contains :: Int -> [Int] -> Bool
 contains x u = x `elem` u
 
+-- 7
 total :: [Int] -> Int
-total u = if length u == 1 then 1 else 1 + total (init u) 
+total u = if length u == 1 then 1 else 1 + total (init u)
 
 total' [] = 0
 total' (x:xs) = 1 + total' xs
 
+-- 8
 bigger :: [Int] -> Int
-bigger u = maximum u
+bigger  = maximum
 
+-- 9
 frequency :: Int -> [Int] -> Int
-frequency x u = length [y | y <- u, x == y] 
+frequency x u = length [y | y <- u, x == y]
 
+-- 10
 unique :: Int -> [Int] -> Bool
 unique x u = length [y | y <- u, x == y] == 1
 
+-- 11
 biggersThan :: Int -> [Int] -> [Int]
 biggersThan x u = [y | y <- u, y > x]
 
+
+-- 12
 concat' :: [Int] -> [Int] -> [Int]
 concat' l1 l2 = l1 ++ l2
 
+-- 13
 tail' :: [Int] -> [Int]
-tail' u = tail u
+tail'  = tail
 
+-- 14
 body :: [Int] -> [Int]
-body u = init u
+body = init
+
+-- 15
+appears :: [Int] -> Int -> Int
+appears u n = length [x | x <- u, x == n]
+
+extract :: Eq a => [a] -> a -> [a]
+extract u n = [x | x <- u, x /= n]
 
 unique' :: [Int] -> [Int]
 unique' [] = []
-unique' (x:xs) = if x `notElem` xs then [x] ++ unique' xs else unique' xs
+unique' (x:xs) = x: unique' (extract xs x)
 
--- minors :: Int -> [Int] -> [Int]
--- minors n (x:xs) = if length minors n == n   
 
--- alter :: Int -> [Int]
--- alter n = [(-x) / (-1) | x <- [1..n]]
+-- 16
 
+minors :: Int -> [Int] -> [Int]
+minors n [] = []
+minors n (x:xs) = [a | a <- xs, a <= x] ++ minors n xs
+
+-- 17
+alter :: Int -> [Int]
+alter n = [((-1)^x) * div x 2  | x <- [2..(2 * n + 1)]]
+
+-- 18
 reverse' :: [a] -> [a]
 reverse' [] = []
 reverse' (x:xs) = reverse' xs ++ [x]
 
+-- 19
 divide :: [Int] -> Int -> ([Int], [Int])
-divide u n = (take n u, drop n u)
+divide u n = splitAt n u
 
+
+-- 20
 intercal :: [Int] -> [Int] -> [Int]
-intercal a b = if length a == 1 then [head a] ++ [head b] else intercal (tail a) (tail b) 
+intercal a [] = a
+intercal [] b = b
+intercal (x:xs) b = x:head b:intercal xs (tail b)
+
+-- 21
+union' :: [Int] -> [Int] -> [Int]
+union' a b = unique' (a ++ b)
+
+-- 22
+-- intersec :: [Int] -> [Int] -> [Int]
+-- intersec a [] = a
+-- intersec [] b = b
+-- intersec a@(x:xs) b@(y:ys) = if appears (unique' (a ++ b)) x > 1
+--                       then x:intersec xs ys
+--                       else if appears (unique' (a ++ b)) y > 1
+--                       then y:intersec xs ys
+--                       else intersec xs ys
+
+-- 23
+
+sequency :: Int -> Int -> [Int]
+sequency n m = [m..(m + n - 1)]
+
+-- 24
+
+insert :: Int -> [Int] -> [Int]
+insert n [] = [n]
+insert n (x:xs) = if n <= x
+                  then n:x:xs
+                  else x:insert n xs
+
+-- 25
+isSorted :: [Int] -> Bool
+isSorted [] = error "Empty List"
+isSorted [a] = True
+isSorted (x:y:xs) = (x <= y) && isSorted (y:xs)
+
+-- 26 
+
+-- qSort :: [Int] -> [Int]
+-- qSort u = u 
+
+-- 27
+-- rotEsq :: Int -> String -> String
+-- rotEsq n s = 
+
+-- 28
+
+-- 29
+-- toUpper c = 
+findIndex :: Char -> Int
+findIndex c = if c `elem` ['a'..'z']
+              then length ['a'..c] - 1
+              else
+                length ['A'..c] - 1
+
+getCorrespondingChar :: Char -> String -> Char
+getCorrespondingChar c s = if c /= ' '
+                              then s !! findIndex c
+                              else ' '
+
+upper :: String -> String
+upper s = [getCorrespondingChar x ['A'..'Z'] | x <- s]
+
+-- 30
+capitalizeFst :: [Char] -> [Char]
+capitalizeFst (x:xs) = getCorrespondingChar x ['A'..'Z'] : xs
+
+title :: String  -> String
+title s = [if c /= ' ' then getCorrespondingChar c ['a'..'z'] else getCorrespondingChar c ['A'..'Z'] | c <- s]
