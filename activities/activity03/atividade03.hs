eb4022b4512071ae6b4d7709a282d5c18940d4c8
joaovictor3g <jvdias79797@gmail.com>
2023-04-03 07:49:52 -0300

    feat: adds more resolutions

--- activities/activity03/atividade03.hs
+++ activities/activity03/atividade03.hs
@@ -34,7 +34,7 @@ popWord all@(x:xs)
   | null all = ("", "")
   | null xs = ([x], "")
   | x == ' ' = ([x], xs)
-  | otherwise = (x: fst (popWord xs), snd (popWord xs))
+  | otherwise = (strip (x: fst (popWord xs)), strip (snd (popWord xs)))
 
 
 
@@ -49,6 +49,7 @@ popWord all@(x:xs)
 breakStr :: [Char] -> [Char]
 breakStr all@(x : xs)
   | x == ' ' = ""
+  | xs == " " = ""
   | null [c | c <- all, c == ' '] = all
   | otherwise = x: breakStr xs
 
@@ -60,3 +61,13 @@ splitStr all@(x:xs)
   | x == ' ' = splitStr (strip xs)
   | otherwise = breakStr all : splitStr (drop pieceLenght xs)
   where pieceLenght = length (breakStr all)
+
+splitStr' :: [Char] -> [[Char]]
+splitStr' xs
+  | null xs = [""]
+  | xs == " " = [""]
+  | otherwise = fst (popWord xs) : splitStr' (snd (popWord xs))
+
+splitStr'' :: [Char] -> [[Char]]
+splitStr'' "" = [""]
+splitStr'' xs = take 1 xs : splitStr'' (drop 1 xs)
